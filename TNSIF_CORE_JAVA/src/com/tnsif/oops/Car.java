4400c561e216e7bbd2b6452278acf959f4707a95
ppuny <ppuny@RahulSarwadha>
2026-07-24 12:51:34 +0530

    Class and Object Concepts in Java

---
+++ TNSIF_CORE_JAVA/src/com/tnsif/oops/Car.java
@@ -0,0 +1,15 @@
+package com.tnsif.oops;
+
+public class Car {
+
+    String color = "Red";
+    String brand = "Toyota";
+
+    void start() {
+        System.out.println("Car Started");
+    }
+
+    void stop() {
+        System.out.println("Car Stopped");
+    }
+}
---
+++ TNSIF_CORE_JAVA/src/com/tnsif/oops/OOPDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.oops;
+
+public class OOPDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Car c = new Car();
+        System.out.println("Brand : " + c.brand);
+        System.out.println("Color : " + c.color);
+
+        c.start();
+        c.stop();
+    }
+
+	}
+
